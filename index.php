d45526a648495727cdedb51f0c03f7e73d312337
Xibalba <jl573160@gmail.com>
2019-09-25 14:53:37 -0500

    Se incluyeron el modelo y el controlador de categorias al index

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-#aver
 session_start();
 require_once "controllers/controller.app.php";
 require_once "controllers/controller.presupuestos.php";
 require_once "controllers/controller.gastos.php";
+require_once "controllers/controller.categorias.php";
 
 require_once "models/model.app.php";
 require_once "models/model.presupuestos.php";
 require_once "models/model.gastos.php";
+require_once "models/model.categorias.php";
 
 $app = new ControlGastosAppCtrl();
 $app -> appControlGastos();
