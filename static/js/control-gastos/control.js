d7d30424fb77722cf4c719a7190f9b3bf67fe86d
Xibalba <jl573160@gmail.com>
2019-09-25 14:45:31 -0500

    Se valido el formulario de registro de categorias

--- static/js/control-gastos/control.js
+++ static/js/control-gastos/control.js
@@ -4,115 +4,132 @@ $("#formLogin").validate({
     correoUsuario: {
       required: true
     },
-    contraseniaUsuario:{
-      required:true
+    contraseniaUsuario: {
+      required: true
     }
   }
 });
 
 /*----------- Validar presupuesto Nuevo -----------*/
 $("#formPresupuestoNuevo").validate({
-  rules:{
-    montoPresupuestoNuevo:{
-      required:true,
+  rules: {
+    montoPresupuestoNuevo: {
+      required: true,
       digits: true
     },
-    formaPagoPresupuestoNuevo:{
-      required:true
+    formaPagoPresupuestoNuevo: {
+      required: true
     },
-    estadoPresupuestoNuevo:{
-      required:true
+    estadoPresupuestoNuevo: {
+      required: true
     }
   }
 });
 
 /*----------- Validar presupuesto Editar -----------*/
 $("#formPresupuestoEditar").validate({
-  rules:{
-    montoPresupuestoEditar:{
-      required:true,
+  rules: {
+    montoPresupuestoEditar: {
+      required: true,
       digits: true
     },
-    formaPagoPresupuestoEditar:{
-      required:true
+    formaPagoPresupuestoEditar: {
+      required: true
     },
-    estadoPresupuestoEditar:{
-      required:true
+    estadoPresupuestoEditar: {
+      required: true
     }
   }
 });
 
 /*----------- Validar presupuesto Eliminar -----------*/
 $("#formPresupuestoEliminar").validate({
-  rules:{
-    keyPresupuestoEliminar:{
-      required:true
+  rules: {
+    keyPresupuestoEliminar: {
+      required: true
     }
   }
 });
 
 /*----------- Validar gasto nuevo -----------*/
 $("#formGastoNuevo").validate({
-  rules:{
-    keyGastoEditar:{
-      required:true
+  rules: {
+    keyGastoEditar: {
+      required: true
     },
-    montoGastoNuevo:{
-      required:true,
+    montoGastoNuevo: {
+      required: true,
       digits: true
     },
-    formaPagoGastoNuevo:{
-      required:true
+    formaPagoGastoNuevo: {
+      required: true
     },
-    productoServicioGastoNuevo:{
-      required:true
+    productoServicioGastoNuevo: {
+      required: true
     },
-    tipoGastoNuevo:{
-      required:true
+    tipoGastoNuevo: {
+      required: true
     },
-    estadoGastoNuevo:{
-      required:true
+    estadoGastoNuevo: {
+      required: true
     }
   }
 });
 
 /*----------- Validar gasto editar -----------*/
 $("#formGastoEditar").validate({
-  rules:{
-    montoGastoEditar:{
-      required:true,
+  rules: {
+    montoGastoEditar: {
+      required: true,
       digits: true
     },
-    formaPagoGastoEditar:{
-      required:true
+    formaPagoGastoEditar: {
+      required: true
     },
-    productoServicioGastoEditar:{
-      required:true
+    productoServicioGastoEditar: {
+      required: true
     },
-    tipoGastoEditar:{
-      required:true
+    tipoGastoEditar: {
+      required: true
     },
-    estadoGastoEditar:{
-      required:true
+    estadoGastoEditar: {
+      required: true
     }
   }
 });
 
 /*----------- Validar gasto Eliminar -----------*/
 $("#formGastoEliminar").validate({
-  rules:{
-    keyGastoEliminar:{
-      required:true
+  rules: {
+    keyGastoEliminar: {
+      required: true
     }
   }
 });
 
+
+/*----------- Validar categorias Nuevo -----------*/
+$("#formCategoriaNuevo").validate({
+  rules: {
+    keyCategoriaNuevo: {
+      required: true
+    },
+    nombreCategoriaNuevo: {
+      required: true
+    },
+    tipoCategoriaNuevo: {
+      required: true
+    }
+  }
+});
 /*=============================================>>>>>
 = Bloque de presupuestos =
 ===============================================>>>>>*/
 
+
+
 /*----------- editar presupuesto -----------*/
-$(".table").on("click",".btnEditarPresupuesto",function(){
+$(".table").on("click", ".btnEditarPresupuesto", function () {
   $(".modal-content-hide").hide();
   $(".loadModal").show();
   $("#formaPagoPresupuestoEditar option:selected").removeAttr("selected");
@@ -120,19 +137,19 @@ $(".table").on("click",".btnEditarPresupuesto",function(){
   var key = $(this).attr("keyPresupuesto");
 
   var datos = new FormData();
-  datos.append("keyPresupuesto",key);
+  datos.append("keyPresupuesto", key);
 
   $.ajax({
-    url:"ajax/ajax.control.php",
-    method:"post",
-    data:datos,
-    cache:false,
-    contentType:false,
-    processData:false,
-    dataType:"json",
-    success:function(respuesta){
+    url: "ajax/ajax.control.php",
+    method: "post",
+    data: datos,
+    cache: false,
+    contentType: false,
+    processData: false,
+    dataType: "json",
+    success: function (respuesta) {
 
-      $.each(respuesta,function(i,o){
+      $.each(respuesta, function (i, o) {
         $("#idPresupuestoEditar").val(key);
         $("#montoPresupuestoEditar").val(o.monto_presupuesto);
         $('#formaPagoPresupuestoEditar option[value="' + o.forma_pago_presupuesto + '"]').attr("selected", true);
@@ -149,7 +166,7 @@ $(".table").on("click",".btnEditarPresupuesto",function(){
 });
 
 /*----------- eliminar presupuesto -----------*/
-$(".table").on("click",".btnEliminarPresupuesto",function(){
+$(".table").on("click", ".btnEliminarPresupuesto", function () {
   var key = $(this).attr("keyPresupuesto");
 
   $("#keyPresupuestoEliminar").val(key);
@@ -166,26 +183,26 @@ $(".table").on("click",".btnEliminarPresupuesto",function(){
 
 /*----------- Editar gasto -----------*/
 
-$(".table").on("click",".btnEditarGasto",function(){
+$(".table").on("click", ".btnEditarGasto", function () {
   $(".modal-content-hide").hide();
   $(".loadModal").show();
   var key = $(this).attr("keyGasto");
 
   var datos = new FormData();
-  datos.append("keyGasto",key);
+  datos.append("keyGasto", key);
 
   $.ajax({
-    url:"ajax/ajax.control.php",
-    method:"POST",
-    data:datos,
-    cache:false,
-    contentType:false,
-    processData:false,
-    dataType:"json",
-    success:function(respuesta){
+    url: "ajax/ajax.control.php",
+    method: "POST",
+    data: datos,
+    cache: false,
+    contentType: false,
+    processData: false,
+    dataType: "json",
+    success: function (respuesta) {
       console.log(respuesta);
       $("#keyGastoEditar").val(key);
-      $.each(respuesta,function(i,o){
+      $.each(respuesta, function (i, o) {
         $("#montoGastoEditar").val(o.monto_gasto);
         $('#formaPagoGastoEditar option[value="' + o.forma_pago_gasto + '"]').attr("selected", true);
         $('#productoServicioGastoEditar').val(o.producto_servicio_gasto);
@@ -202,7 +219,7 @@ $(".table").on("click",".btnEditarGasto",function(){
 });
 
 /*----------- Eliminar gasto -----------*/
-$(".table").on("click",".btnEliminarGasto",function(i,o){
+$(".table").on("click", ".btnEliminarGasto", function (i, o) {
   var key = $(this).attr("keyGasto");
 
   $("#keyGastoEliminar").val(key);
@@ -220,60 +237,60 @@ $(".table").on("click",".btnEliminarGasto",function(i,o){
 $(document).ready(function () {
   var ctx = document.getElementById('myChart').getContext('2d');
   var myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-          labels: ['Presupuesto', 'Gastos'],
-          datasets: [{
-              label: '# of Votes',
-              data: [2000, 700],
-              backgroundColor: [
-                  'rgba(28, 200, 138, 0.5)',
-                  'rgba(231, 74, 59, 0.5)',
-              ],
-              borderColor: [
-                  'rgba(28, 200, 138, 1)',
-                  'rgba(231, 74, 59, 1)',
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-      }
+    type: 'pie',
+    data: {
+      labels: ['Presupuesto', 'Gastos'],
+      datasets: [{
+        label: '# of Votes',
+        data: [2000, 700],
+        backgroundColor: [
+          'rgba(28, 200, 138, 0.5)',
+          'rgba(231, 74, 59, 0.5)',
+        ],
+        borderColor: [
+          'rgba(28, 200, 138, 1)',
+          'rgba(231, 74, 59, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+    }
   });
 
   var ctt = document.getElementById('myChart2').getContext('2d');
   var mixedChart = new Chart(ctt, {
     type: 'bar',
     data: {
-        datasets: [{
-            label: 'Presupuestos por mes',
-            data: [10, 95, 30, 40],
-            backgroundColor: [
-                'rgba(28, 200, 138, 0.5)',
-            ],
-            borderColor: [
-                'rgba(28, 200, 138, 1)',
-            ],
-            type: 'line'
-        }, {
-            label: 'Gastos por mes',
-            data: [15, 59, 50, 50],
-            backgroundColor: [
-                'rgba(231, 74, 59, 0.5)',
-            ],
-            borderColor: [
-                'rgba(231, 74, 59, 1)',
-            ],
+      datasets: [{
+        label: 'Presupuestos por mes',
+        data: [10, 95, 30, 40],
+        backgroundColor: [
+          'rgba(28, 200, 138, 0.5)',
+        ],
+        borderColor: [
+          'rgba(28, 200, 138, 1)',
+        ],
+        type: 'line'
+      }, {
+        label: 'Gastos por mes',
+        data: [15, 59, 50, 50],
+        backgroundColor: [
+          'rgba(231, 74, 59, 0.5)',
+        ],
+        borderColor: [
+          'rgba(231, 74, 59, 1)',
+        ],
 
-            // Changes this dataset to become a line
-            type: 'line'
-        }],
-        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio' , 'Julio' , 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+        // Changes this dataset to become a line
+        type: 'line'
+      }],
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
     },
     options: {
 
     }
-});
+  });
 });
 
 /*= End of Bloque de inicio =*/
