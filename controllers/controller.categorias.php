0c2a56fe93367ce53136772f144787cff57a3180
Xibalba <jl573160@gmail.com>
2019-09-25 15:32:49 -0500

    Se agrego la fecha de registro de la categoria

--- controllers/controller.categorias.php
+++ controllers/controller.categorias.php
@@ -5,10 +5,16 @@ class CategoriasCtrl{
   ===============================================>>>>>*/
   static public function agregarCategoria(){
     if (isset($_POST["keyCategoriaNuevo"])) {
+
+      date_default_timezone_set('America/Mexico_City');
+      $time = mktime();
+      $fecha = date("Y-m-d h:i:s a", $time);
+
       $datos = array(
         "nombre"=>$_POST["nombreCategoriaNuevo"],
         "tipo"=>$_POST["tipoCategoriaNuevo"],
         "descripcion"=>$_POST["descripcionCategoriaNuevo"],
+        "fecha"=>$fecha,
         "key"=>ControlGastosAppCtrl::openCypher("decrypt",$_POST["keyCategoriaNuevo"])
       );
 
--- models/model.categorias.php
+++ models/model.categorias.php
@@ -4,10 +4,11 @@ class CategoriasMdl extends Conexion{
     = Agregar nueva categoria =
     ===============================================>>>>>*/
     static public function agregarCategoria($datos,$tabla){
-      $stmt=Conexion::conectar()->prepare("INSERT INTO $tabla (nom_categoria,tipo_categoria,descripcion_categoria,id_usuario) VALUES (:nombre,:tipo,:descripcion,:key)");
+      $stmt=Conexion::conectar()->prepare("INSERT INTO $tabla (nom_categoria,tipo_categoria,descripcion_categoria,fecha_categoria,id_usuario) VALUES (:nombre,:tipo,:descripcion,:fecha,:key)");
       $stmt->bindParam(":nombre",$datos["nombre"],PDO::PARAM_STR);
       $stmt->bindParam(":tipo",$datos["tipo"],PDO::PARAM_STR);
       $stmt->bindParam(":descripcion",$datos["descripcion"],PDO::PARAM_STR);
+      $stmt->bindParam(":fecha",$datos["fecha"],PDO::PARAM_STR);
       $stmt->bindParam(":key",$datos["key"],PDO::PARAM_STR);
       return ($stmt->execute()) ? 'exito':'error';
       $stmt->close();
