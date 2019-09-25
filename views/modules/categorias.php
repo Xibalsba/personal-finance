3002c0b86b62cb3135e160ad64b0b38c5f114c60
Xibalba <jl573160@gmail.com>
2019-09-25 13:24:57 -0500

    Se anexo la visualizacion de las categorias

--- views/modules/categorias.php
+++ views/modules/categorias.php
@@ -1,1 +1,65 @@
-<h2>Catergorias</h2>
+<div class="row">
+  <div class="col-xl-12 col-md-12 mb-2 clearfix">
+    <h2 class="h3 text-gray-800">CATEGORIAS</h2>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalGastoNuevo"><i class="fa fa-plus mr-2"></i>Agregar nueva categoria</button>
+  </div>
+  <div class="col-xl-12 col-md-12 clearfix">
+    <?php
+
+    ?>
+  </div>
+  <div class="col-xl-12 col-md-12 mb-4">
+    <div class="card">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card" style="border:none;">
+            <div class="card-body">
+              <div class="d-flex align-items-center justify-content-center flex-column flex-sm-row">
+                <i class="mdi mdi-clipboard-text mdi-36px text-success mr-0 mr-sm-4"></i>
+                <div class="wrapper text-center text-sm-left">
+                  <h5 class="card-title">Categorias Presupuesto</h5>
+                  <div class="fluid-container">
+                    <h3 class="mb-0 font-weight-medium">12
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card" style="border:none;">
+            <div class="card-body">
+              <div class="d-flex align-items-center justify-content-center flex-column flex-sm-row">
+                <i class="mdi mdi-clipboard-text mdi-36px text-danger mr-0 mr-sm-4"></i>
+                <div class="wrapper text-center text-sm-left">
+                  <h5 class="card-title">Categorias Gasto</h5>
+                  <div class="fluid-container">
+                    <h3 class="mb-0 font-weight-medium">12
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <table class="table table-striped display responsive nowrap" style="width:100%">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Descripcion</th>
+              <th scope="col">Fecha</th>
+              <th scope="col">Tipo</th>
+              <th scope="col">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
