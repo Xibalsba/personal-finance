24272f3a8ea1a813dd4fb266636b9cdf9febde1c
Xibalba <jl573160@gmail.com>
2019-10-05 10:13:58 -0500

    Mostrar la imagen del usuario en el topbar

--- views/system/topbar.php
+++ views/system/topbar.php
@@ -21,7 +21,7 @@
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="mr-2 d-none d-lg-inline text-gray-600 small">Julio López</span>
-        <img class="img-profile rounded-circle" src="http://www.adventuresinpoortaste.com/wp-content/uploads/2017/04/batman-vol-2-rebirth-featured.jpg">
+        <img class="img-profile rounded-circle" src="<?php echo $_SESSION["userFt"]; ?>">
         <div class="status-indicator bg-success"></div>
       </a>
       <!-- Dropdown - User Information -->
