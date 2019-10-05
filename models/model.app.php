37fd4e54e35964bf8ca365e47e21af9c231e377d
Xibalba <jl573160@gmail.com>
2019-10-05 08:42:19 -0500

    Se corrigo el bug de la conexion con el model al iniciar sesion

--- models/model.app.php
+++ models/model.app.php
@@ -15,14 +15,6 @@ class ControlGastosAppMdl extends Conexion{
 		}
 		return $page;
 	}
-
-  static public function iniciarSesion($correo,$tabla){
-    $stmt=Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE correo_usuario=:correo");
-    $stmt->bindParam(":correo",$correo,PDO::PARAM_STR);
-    $stmt->execute();
-    return $stmt->fetchAll();
-    $stmt->close();
-  }
 }
 
 ?>
--- models/model.user.php
+++ models/model.user.php
@@ -3,7 +3,13 @@
 require_once "model.config.php";
 
 class UsuariosMdl extends Conexion{
-
+  static public function iniciarSesion($correo,$tabla){
+    $stmt=Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE correo_usuario=:correo");
+    $stmt->bindParam(":correo",$correo,PDO::PARAM_STR);
+    $stmt->execute();
+    return $stmt->fetchAll();
+    $stmt->close();
+  }
 }
 
 ?>
