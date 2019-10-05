de508abc4550a69b0801295b34756bdfc30954ee
Xibalba <jl573160@gmail.com>
2019-10-05 10:00:27 -0500

    Se corrigio el bug del model.categorias not found

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once "controllers/controller.user.php";
 require_once "models/model.app.php";
 require_once "models/model.presupuestos.php";
 require_once "models/model.gastos.php";
+require_once "models/model.categorias.php";
 require_once "models/model.user.php";
 
 $app = new ControlGastosAppCtrl();
